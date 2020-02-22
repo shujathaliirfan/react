72aed63ecb08b3960f80340043202f800aefd7b1
shujathaliirfan <shujathali92@gmail.com>
2020-02-22 23:13:48 +0530

    basics of react as of 22/02/2020

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Person from './Person/Person';
+
 import './App.css';
 
-function App() {
+class App extends Component {
+
+
+
+  state ={
+    Persons:[
+      {name:"shujath", age:"22"},
+      {name:"alih", age:"28"},
+      {name:"irfan", age:"29"}
+
+
+
+    ]
+  }
+
+myFunction = (newName) =>{
+  this.setState({
+    Persons:[
+      {name:"kamran", age:"266"},
+      {name:newName, age:"666"},
+      {name:"muzammil", age:"88"}
+    ]
+
+  })
+}
+
+
+
+changed =(event)=>{
+  this.setState({
+    Persons:[
+      {name:event.target.value, age:"266"},
+      {name:'max', age:"666"},
+      {name:"muzammil", age:"88"}
+    ]
+
+  })
+
+}
+render(){
+  const mee={
+    fontSize:'16px',
+    padding:'9px',
+    backgroundColor:'yellow',
+    color:'black'
+
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  <div className="App">
+    <h1>iam  React a developer</h1>
+
+    <button style={mee} onClick={this.myFunction.bind(this,'khaaali')}>click me</button>
+
+    <Person changed={this.changed} click={this.myFunction.bind(this,"pathan")}  name={this.state.Persons[0].name} age={this.state.Persons[0].age}  currentName/>
+    <Person   name={this.state.Persons[1].name} age={this.state.Persons[1].age} />
+    <Person name={this.state.Persons[2].name} age={this.state.Persons[2].age} />
+
+
+
+
+
+
+  </div> 
+   )}
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import './Person.css'
+
+var Person =(props) =>{
+return(
+<div className="Person">
+<p>I'm a {props.name} react developer  and iam just {props.age}</p>
+ {/* <p onClick={props.click}>my secomd p re beta maafi chaahta hoon</p>9 */}
+ <input type="text" onChange={props.changed} value={props.currentName} />
+</div>
+ ) 
+}
+
+export default Person;
